6598dbc00c18bf95006550fdbd11902a3d2a43a0
Zach Nowlin <zach@zachbyte.com>
2024-10-31 10:24:41 -0600

    Update server-setup.sh

--- arch/server-setup.sh
+++ arch/server-setup.sh
@@ -654,7 +654,7 @@ echo -ne "
 
 # Change to the user's home directory and run the command within the chroot environment
 arch-chroot /mnt /bin/bash -c "
-    cd /home/$USERNAME
+    cd /home/$username
     curl -fsSL https://github.com/zachbyte/archbyte/raw/main/install.sh | sh
 "
 
